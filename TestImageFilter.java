1059031ff7fd324927fbef44b3b92f8a28d81280
Christian Lascsak <christian.lascsak7@gmail.com>
2021-10-16 15:22:00 +0200

    add parallelFilter execution

--- TestImageFilter.java
+++ TestImageFilter.java
@@ -1,5 +1,6 @@
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import javax.imageio.IIOException;
 
 import javax.imageio.ImageIO;
@@ -30,18 +31,23 @@ public class TestImageFilter {
 		int h = image.getHeight();
 		System.out.println("Image size is " + w + "x" + h);
 		System.out.println();
-	
+
 		int[] src = image.getRGB(0, 0, w, h, null, 0, w);
 		int[] dst = new int[src.length];
 
+		startSequentialFilter(srcFileName, w, h, src, dst);
+		startParallelFilter(srcFileName, w, h, src, dst);
+	}
+
+	private static void startSequentialFilter(String srcFileName, int w, int h, int[] src, int[] dst) throws IOException {
 		System.out.println("Starting sequential image filter.");
 
 		long startTime = System.currentTimeMillis();
-		ImageFilter filter0 = new ImageFilter(src, dst, w, h);
-		filter0.apply();
+		ImageFilter filter = new ImageFilter(src, dst, w, h);
+		filter.apply();
 		long endTime = System.currentTimeMillis();
 
-		long tSequential = endTime - startTime; 
+		long tSequential = endTime - startTime;
 		System.out.println("Sequential image filter took " + tSequential + " milliseconds.");
 
 		BufferedImage dstImage = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
@@ -51,6 +57,29 @@ public class TestImageFilter {
 		File dstFile = new File(dstName);
 		ImageIO.write(dstImage, "jpg", dstFile);
 
-		System.out.println("Output image: " + dstName);	
+		System.out.println("Output image: " + dstName);
 	}
+
+	private static void startParallelFilter(String srcFileName, int w, int h, int[] src, int[] dst) throws IOException {
+		System.out.println("Starting parallel image filter.");
+
+		long startTime = System.currentTimeMillis();
+		ParallelFJImageFilter filter = new ParallelFJImageFilter(src, dst, w, h);
+		filter.apply(2);
+		long endTime = System.currentTimeMillis();
+
+		long dt = endTime - startTime;
+		System.out.println("Parallel image filter took " + dt + " milliseconds.");
+
+		BufferedImage dstImage = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+		dstImage.setRGB(0, 0, w, h, dst, 0, w);
+
+		String dstName = "Filtered_Parallel" + srcFileName;
+		File dstFile = new File(dstName);
+		ImageIO.write(dstImage, "jpg", dstFile);
+
+		System.out.println("Output image: " + dstName);
+	}
+
+
 }
