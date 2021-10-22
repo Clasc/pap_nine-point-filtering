7d5ef7e675725aa7dc06135bbb96d1dde29a8be3
Christian Lascsak <christian.lascsak7@gmail.com>
2021-10-22 14:49:01 +0200

    update run and build scripts

--- run.sh
+++ run.sh
@@ -1,1 +1,15 @@
-javac *.java -d out
+#!/bin/bash
+if [ -z $1 ]; then
+    echo "no image file specified, setting to IMAGE1.JPG"
+    input=IMAGE1.JPG
+    else
+      input=$1
+fi
+
+if [ -z $2 ]; then
+    echo "no out file specified, setting to out.txt"
+    output=out.txt
+      else
+      output=$2
+fi
+java -classpath out/ TestImageFilter $input $output
